281c4b0622b17fe948d02dc33f7a02b1222588b3
yuki takagi <yuki99takagi@gmail.com>
2020-06-04 15:45:16 +0900

    Add : practice_diaryモデルを作成 #3

---
+++ app/models/practice_diary.rb
@@ -0,0 +1,2 @@
+class PracticeDiary < ApplicationRecord
+end
---
+++ test/models/practice_diary_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PracticeDiaryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
