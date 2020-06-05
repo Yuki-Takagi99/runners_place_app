7687c3160e65c2d1fd00c08ffa11f6955983d9fd
yuki takagi <yuki99takagi@gmail.com>
2020-06-05 10:46:15 +0900

    Add : 練習記録の編集機能を作成 #3

--- app/controllers/practice_diaries_controller.rb
+++ app/controllers/practice_diaries_controller.rb
@@ -1,4 +1,5 @@
 class PracticeDiariesController < ApplicationController
+	before_action :set_practice_diary, only: [:show, :edit, :update]
 	def index
 		@practice_diaries = PracticeDiary.all
 	end
@@ -12,17 +13,33 @@ class PracticeDiariesController < ApplicationController
 		if @practice_diary.save
 			redirect_to @practice_diary, notice: "練習記録を作成しました!"
 		else
+			flash.now[:alert] = "練習記録の投稿に失敗しました。"
 			render :new
 		end
 	end
 
-		def show
-			@practice_diary = PracticeDiary.find(params[:id])
+	def show
+	end
+
+	def edit
+	end
+	
+	def update
+		if @practice_diary.update(practice_diary_params)
+			redirect_to practice_diary_path, notice: "練習記録を編集しました！"
+		else
+			flash.now[:alert] = "練習記録の編集に失敗しました。"
+			render :edit
 		end
+	end
+	
 
 	private
-
 	def practice_diary_params
 		params.require(:practice_diary).permit(:practice_date, :practice_title, :practice_content, :practice_distance, :practice_time)
+	end
+
+	def set_practice_diary
+		@practice_diary = PracticeDiary.find(params[:id])
 	end
 end
