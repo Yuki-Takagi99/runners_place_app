8edb5549a06794f1629fcd177e35551300ab1089
yuki takagi <yuki99takagi@gmail.com>
2020-06-05 13:10:54 +0900

    Update : 走行時間表示を成型 #3

--- app/models/practice_diary.rb
+++ app/models/practice_diary.rb
@@ -2,4 +2,10 @@ class PracticeDiary < ApplicationRecord
 	validates :practice_title, presence: true
 	validates :practice_content, presence: true
 	validates :practice_distance, presence: true
+	validates :practice_time, presence: true
+
+	# 練習時間表示の成型
+	def set_practice_time
+		practice_time.strftime("%-H時間%M分%S秒")
+	end
 end
