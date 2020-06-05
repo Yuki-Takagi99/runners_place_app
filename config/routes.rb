eace2a3bc54038267d8cbe9106ed7744bcb2f1d0
yuki takagi <yuki99takagi@gmail.com>
2020-06-05 16:54:03 +0900

    Add : deviseの初期設定 #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'practice_diaries#index'
   resources :practice_diaries
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
