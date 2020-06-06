c95a132b3c099d67a105bf5be98772c53ca5c4e1
yuki takagi <yuki99takagi@gmail.com>
2020-06-06 15:40:07 +0900

    Add : Userテーブルに名前、自己紹介、目標カラム追加 #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_080330) do
+ActiveRecord::Schema.define(version: 2020_06_06_061507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2020_06_05_080330) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name", default: "", null: false
+    t.text "self_introduction", default: "", null: false
+    t.text "target", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
