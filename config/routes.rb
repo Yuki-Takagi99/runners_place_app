31b726627a90aaeb3cc4269605fa4a96197a7bbd
yuki takagi <yuki99takagi@gmail.com>
2020-06-06 16:56:26 +0900

    Add : ユーザプロフィール画面を作成 #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'practice_diaries#index'
+  devise_for :users
+  resources :users, only: [:show]
   resources :practice_diaries
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
