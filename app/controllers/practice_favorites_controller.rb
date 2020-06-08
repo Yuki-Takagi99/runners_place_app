bff3e31321e21ef4488913729defe3d3c881a851
yuki takagi <yuki99takagi@gmail.com>
2020-06-08 17:47:05 +0900

    Add : お気に入り機能のシステムスペックテストを作成 #9

--- app/controllers/practice_favorites_controller.rb
+++ app/controllers/practice_favorites_controller.rb
@@ -1,7 +1,7 @@
 class PracticeFavoritesController < ApplicationController
 	def create
 		practice_favorite = current_user.practice_favorites.create(practice_diary_id: params[:practice_diary_id])
-    redirect_to "/practice_diaries/#{params[:practice_diary_id]}", notice: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り登録しました！"
+    redirect_to practice_diaries_url, notice: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り登録しました！"
 	end
 	
 	def destroy
