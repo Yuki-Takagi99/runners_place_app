c2de1564bfdf1f2eab0ea2c2dd66814e2f9d8542
yuki takagi <yuki99takagi@gmail.com>
2020-06-08 17:59:14 +0900

    Add : お気に入り機能のモデルスペックテストを作成

--- spec/models/practice_favorite_spec.rb
+++ spec/models/practice_favorite_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PracticeFavorite, type: :model do
-  describe 'バリデーションテスト' do
-    it '' do
+	describe 'バリデーションテスト' do
+		before do
+			@user = create(:user)
+			@practice_diary = create(:practice_diary, user_id: @user.id)
+			@practice_favorite = create(:practice_favorite, user_id: @user.id, practice_diary_id: @practice_diary.id)
+			@practice_favorite2 = build(:practice_favorite, user_id: @user.id, practice_diary_id: @practice_diary.id)
+		end
+		it 'データがあれば有効であること' do
+			expect(@practice_favorite).to be_valid
+		end
+
+		it '重複してお気に入り登録すると無効であること' do
+			expect(@practice_favorite2).not_to be_valid
+		end
+
+		it 'user_idがなければ無効であること' do
+			@practice_favorite.user_id = nil
+			expect(@practice_favorite).not_to be_valid
+		end
+		
+		it 'practice_diary_idがなければ無効であること' do
+			@practice_favorite.practice_diary_id = nil
+			expect(@practice_favorite).not_to be_valid
 		end
 	end
 end
