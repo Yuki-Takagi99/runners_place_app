e775f7693743c1a6920e9a29eb8a59ac613d63cc
yuki takagi <yuki99takagi@gmail.com>
2020-06-10 09:01:20 +0900

    Add : フラッシュメッセージのbootstrap対応 #13

--- app/controllers/practice_favorites_controller.rb
+++ app/controllers/practice_favorites_controller.rb
@@ -1,11 +1,11 @@
 class PracticeFavoritesController < ApplicationController
 	def create
 		practice_favorite = current_user.practice_favorites.create(practice_diary_id: params[:practice_diary_id])
-    redirect_to practice_diaries_url, notice: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り登録しました！"
+    redirect_to practice_diaries_url, success: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り登録しました！"
 	end
 	
 	def destroy
 		practice_favorite = current_user.practice_favorites.find_by(id: params[:id]).destroy
-    redirect_to practice_diaries_url, notice: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り解除しました！"
+    redirect_to practice_diaries_url, info: "#{practice_favorite.practice_diary.user.user_name}さんの練習記録をお気に入り解除しました！"
 	end
 end
