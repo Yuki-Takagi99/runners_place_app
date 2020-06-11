32a9c12eef166adb92bb3a89b6130aed0cf7b591
yuki takagi <yuki99takagi@gmail.com>
2020-06-11 11:44:16 +0900

    Add : コメント編集ボタンを作成 #15

--- app/controllers/practice_comments_controller.rb
+++ app/controllers/practice_comments_controller.rb
@@ -1,4 +1,5 @@
 class PracticeCommentsController < ApplicationController
+  before_action :set_practice_diary, only: [:create, :edit, :update]
   def create
     @practice_diary = PracticeDiary.find(params[:practice_diary_id])
     @practice_comment = @practice_diary.practice_comments.build(practice_comment_params)
@@ -12,9 +13,37 @@ class PracticeCommentsController < ApplicationController
     end
   end
 
+  def edit
+    @practice_comment = @practice_diary.practice_comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @practice_comment = @practice_diary.practice_comments.find(params[:id])
+    respond_to do |format|
+      if @practice_comment.update(practice_comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit_error }
+      end
+    end
+  end
+  
+  
+
   private
   def practice_comment_params
     params.require(:practice_comment).permit(:practice_diary_id, :practice_comment_content)
   end
+
+  def set_practice_diary
+    @practice_diary = PracticeDiary.find(params[:practice_diary_id])
+  end
+  
 
 end
