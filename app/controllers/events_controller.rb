a6db047b0c66cdb9679e1fe0f4ac8a994830807f
yuki takagi <yuki99takagi@gmail.com>
2020-06-16 10:51:51 +0900

    Add : イベント詳細画面に現在の参加者数を表示 #25

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,9 @@ class EventsController < ApplicationController
 
   def show
     @event_comments = @event.event_comments
-		@event_comment = @event.event_comments.build
+    @event_comment = @event.event_comments.build
+    # イベント参加者の一覧を取得
+    @participant_users = @event.participant_users
   end
 
   def new
