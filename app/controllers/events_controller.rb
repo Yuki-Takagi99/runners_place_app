3d445096be391bb23b6ba5903380a4274aeb7734
yuki takagi <yuki99takagi@gmail.com>
2020-06-16 21:12:40 +0900

    Add : イベント一覧ページにページネーション を追加 #27

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all.recent
+    @events = Event.all.recent.page(params[:page]).per(30)
   end
 
   def show
