c80f8406ba4f8612df8de5ff7d17076a260d65f5
yuki takagi <yuki99takagi@gmail.com>
2020-06-19 20:47:59 +0900

    Update : Userモデルのテストを修正 #38

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,43 +6,43 @@ RSpec.describe User, type: :model do
   end
 
   describe 'バリデーションテスト' do
-    it '名前、メールアドレス、パスワードがある場合、有効であること' do
+    it 'user_name、email、passwordがある場合、有効であること' do
       expect(@user).to be_valid
     end
 
-    it '名前がない場合、無効であること' do
+    it 'user_nameがない場合、無効であること' do
       @user.user_name = nil
       expect(@user).not_to be_valid
     end
 
-    it 'メールアドレスがない場合、無効であること' do
+    it 'emailがない場合、無効であること' do
       @user.email = nil
       expect(@user).not_to be_valid
     end
 
-    it 'パスワードがない場合、無効であること' do
+    it 'passwordがない場合、無効であること' do
       @user.password = nil
       expect(@user).not_to be_valid
     end
 
-    it '31文字以上の名前の場合、無効であること' do
+    it '31文字以上のuser_nameの場合、無効であること' do
       @user.user_name = 'a' * 31
       expect(@user).not_to be_valid
     end
 
-    it '256文字以上のメールアドレスの場合、無効であること' do
+    it '256文字以上のemailの場合、無効であること' do
       @user.email = "#{'a' * 250}@a.com"
       expect(@user).not_to be_valid
     end
 
-    it '重複したメールアドレスの場合、無効であること' do
+    it '重複したemailの場合、無効であること' do
       @user2 = create(:user_2)
       @user3 = create(:user_3)
       @user3.email = "unique@example.com"
       expect(@user3).not_to be_valid
     end
 
-    it '6文字未満のパスワードを入力した場合、無効であること' do
+    it '6文字未満のpasswordを入力した場合、無効であること' do
       @user.password = 1 * 5
       expect(@user).not_to be_valid
     end
