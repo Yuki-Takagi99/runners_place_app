83ac4865195bfd1b915cdcfd583b1eeda02d6f6a
yuki takagi <yuki99takagi@gmail.com>
2020-06-19 21:19:37 +0900

    Update : PracticeFavoriteモデルのテストを修正 #38

--- spec/models/practice_favorite_spec.rb
+++ spec/models/practice_favorite_spec.rb
@@ -8,20 +8,21 @@ RSpec.describe PracticeFavorite, type: :model do
 			@practice_favorite = create(:practice_favorite, user_id: @user.id, practice_diary_id: @practice_diary.id)
 			@practice_favorite2 = build(:practice_favorite, user_id: @user.id, practice_diary_id: @practice_diary.id)
 		end
-		it 'データがあれば有効であること' do
+
+		it 'すべてのデータがある場合、有効であること' do
 			expect(@practice_favorite).to be_valid
 		end
 
-		it '重複してお気に入り登録すると無効であること' do
+		it '重複してお気に入り登録した場合、無効であること' do
 			expect(@practice_favorite2).not_to be_valid
 		end
 
-		it 'user_idがなければ無効であること' do
+		it 'user_idがない場合、無効であること' do
 			@practice_favorite.user_id = nil
 			expect(@practice_favorite).not_to be_valid
 		end
-		
-		it 'practice_diary_idがなければ無効であること' do
+
+		it 'practice_diary_idがない場合、無効であること' do
 			@practice_favorite.practice_diary_id = nil
 			expect(@practice_favorite).not_to be_valid
 		end
