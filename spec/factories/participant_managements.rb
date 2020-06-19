5081900698b30d27f271b138389f30941bf3a561
yuki takagi <yuki99takagi@gmail.com>
2020-06-19 21:58:03 +0900

    Add : ParticipantManagementモデルスペックテストを作成 #38

--- spec/factories/participant_managements.rb
+++ spec/factories/participant_managements.rb
@@ -1,6 +1,4 @@
 FactoryBot.define do
   factory :participant_management do
-    user { nil }
-    event { nil }
   end
 end
--- spec/models/participant_management_spec.rb
+++ spec/models/participant_management_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ParticipantManagement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションテスト' do
+    before do
+      @user = create(:user)
+      @event = create(:event, user_id: @user.id)
+      @participant_management = create(:participant_management, user_id: @user.id, event_id: @event.id)
+    end
+
+    it 'すべてのデータがある場合、有効であること' do
+			expect(@participant_management).to be_valid
+		end
+
+		it 'user_idがない場合、無効であること' do
+			@participant_management.user_id = nil
+			expect(@participant_management).not_to be_valid
+		end
+
+		it 'event_idがない場合、無効であること' do
+			@participant_management.event_id = nil
+			expect(@participant_management).not_to be_valid
+		end
+  end
 end
