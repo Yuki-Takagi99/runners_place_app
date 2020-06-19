ada94b1bf7863fee6635a54d9ddd1f428f5c64c3
yuki takagi <yuki99takagi@gmail.com>
2020-06-20 08:55:02 +0900

    Fix : システムスペックテストの際、アイコンが表示されないエラーを解消 #38

--- spec/system/practice_diary_spec.rb
+++ spec/system/practice_diary_spec.rb
@@ -1,42 +1,21 @@
 require 'rails_helper'
-RSpec.describe "練習記録管理機能", type: :system do
-	describe '練習記録管理' do
-		before do
-			@user = create(:user)
-		end
 
-		context '練習記録作成、編集、削除機能' do
-			it '練習記録が作成できること' do
-				visit new_user_session_path
-				fill_in 'Eメール', with: 'test1@example.com'
-				fill_in 'パスワード', with: 'password'
-				click_button 'ログイン'
-				click_on '練習記録をつける'
-				select_date("2019,12,31", from: "練習日")
-				fill_in 'タイトル', with: '作成テスト'
-				fill_in '詳細', with: '作成テスト'
-				fill_in '走行距離', with: '10.0'
-				select_time("01", "50", "30", from: "走行時間")
-				click_on '登録する'
-				expect(page).to have_content '作成テスト'
-			end
+RSpec.describe 'PracticeDiary', type: :system do
+	let!(:user) { create(:user) }
+	let!(:other_user) { create(:user) }
 
-			it '作成した練習記録が編集できること' do
-				visit new_user_session_path
-				fill_in 'Eメール', with: 'test2@example.com'
-				fill_in 'パスワード', with: 'password'
-				click_button 'ログイン'
-				click_on '練習記録をつける'
-				select_date("2019,12,31", from: "練習日")
-				fill_in 'タイトル', with: '作成テスト'
-				fill_in '詳細', with: '作成テスト'
+	describe '練習記録の作成' do
+		context 'データが正しく入力されている場合' do
+			it '練習記録が作成でき、練習記録詳細画面に遷移すること' do
+				login(user)
+				click_on '記録をつける', match: :first
+				fill_in '練習日', with: Date.today - 1
+				fill_in 'タイトル', with: 'ジョギング'
+				fill_in '詳細', with: 'ゆっくり走りました！'
 				fill_in '走行距離', with: '10.0'
-				select_time("01", "50", "30", from: "走行時間")
+				select_time("01", "00", "30", from: "走行時間")
 				click_on '登録する'
-				click_on '編集する'
-				fill_in 'タイトル', with: '編集テスト'
-				click_on '更新する'
-				expect(page).to have_content '編集テスト'
+				expect(page).to have_content 'ジョギング'
 			end
 		end
 	end
