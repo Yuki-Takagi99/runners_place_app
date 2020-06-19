68345d02b4bb65de39f8e4db9725a461fd0ff675
yuki takagi <yuki99takagi@gmail.com>
2020-06-19 21:42:27 +0900

    Update: EventCommentモデルスペックテストを修正 #38

--- spec/factories/event_comments.rb
+++ spec/factories/event_comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :event_comment do
-    event_comment_content { "comment_test" }
+    event_comment_content { "テスト用コメント" }
   end
 end
--- spec/models/event_comment_spec.rb
+++ spec/models/event_comment_spec.rb
@@ -6,27 +6,28 @@ RSpec.describe EventComment, type: :model do
 			@user = create(:user)
 			@event = create(:event, user_id: @user.id)
 			@event_comment = create(:event_comment, user_id: @user.id, event_id: @event.id)
-		end
-		it 'データがあれば有効であること' do
+    end
+
+		it 'すべてのデータがある場合、有効であること' do
 			expect(@event_comment).to be_valid
     end
 
-    it 'event_comment_contentがなければ無効であること' do
+    it 'event_comment_contentがない場合、無効であること' do
       @event_comment.event_comment_content = nil
       expect(@event_comment).not_to be_valid
     end
 
-    it 'event_comment_contentが151文字以上なら無効であること' do
+    it 'event_comment_contentが151文字以上の場合、無効であること' do
       @event_comment.event_comment_content = "a" * 151
       expect(@event_comment).not_to be_valid
     end
 
-    it 'user_idがなければ無効であること' do
+    it 'user_idがない場合、無効であること' do
       @event_comment.user_id = nil
       expect(@event_comment).not_to be_valid
     end
 
-    it 'event_idがなければ無効であること' do
+    it 'event_idがない場合、無効であること' do
       @event_comment.event_id = nil
       expect(@event_comment).not_to be_valid
     end
