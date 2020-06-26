b3636f5ab1822403789837c03eee44d34c5acfa6
yuki takagi <yuki99takagi@gmail.com>
2020-06-26 11:42:37 +0900

    Update : helperメソッドの命名を変更 #63

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # practice_timeがtime型のため、下記メソッド定義
-  # 基本はUTC,投稿日時などを表示する際にlocalizeメソッドでtime_zone設定を'Asia,Tokyo'に変更する
-  def localize(time, zone)
+  # タイムゾーン設定はUTCのため,投稿日時などを表示する際にlocalize_helperでtime_zone設定を'Asia,Tokyo'に変更する
+  def localize_helper(time, zone)
     I18n.l time.in_time_zone(zone)
   end
 
