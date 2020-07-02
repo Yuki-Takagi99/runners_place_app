9611270329f3a79aaceebf0827a5375db7895b86
yuki takagi <yuki99takagi@gmail.com>
2020-07-02 20:32:07 +0900

    Add : 通知機能のテストを追加 #67

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,10 +1,4 @@
 FactoryBot.define do
   factory :notification do
-    visitor { nil }
-    visited { nil }
-    practice_diary { nil }
-    practice_comment { nil }
-    action { "MyString" }
-    checked { false }
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションテスト" do
+    before do
+      @user = create(:user)
+      @user2 = create(:jon)
+      @practice_diary = create(:practice_diary, user_id: @user.id)
+      @practice_comment = create(:practice_comment, user_id: @user.id, practice_diary_id: @practice_diary.id)
+      @notification = create(:notification, visitor_id: @user.id, visited_id: @user2.id, practice_diary_id: @practice_diary.id, practice_comment_id: @practice_comment.id, action: "comment", checked: "false")
+    end
+
+    it 'すべてのデータがある場合、有効であること' do
+      expect(@notification).to be_valid
+    end
+
+    it 'visitor_idがない場合、無効であること' do
+      @notification.visitor_id = nil
+      expect(@notification).not_to be_valid
+    end
+
+    it 'visited_idがない場合、無効であること' do
+      @notification.visited_id = nil
+      expect(@notification).not_to be_valid
+    end
+  end
 end
